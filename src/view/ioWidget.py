4acd5be9f8b6275aa5703a38e052427f61b53b16
Oliver Köhler <olk.git@posteo.eu>
2021-07-20 16:53:27 +0200

    added file dialogs

--- src/view/ioWidget.py
+++ src/view/ioWidget.py
@@ -38,14 +38,14 @@ class IOWidget(QWidget):
         self.ioFrameLayout.addWidget(self.ioLabel)
         self.ioFormLayout.setWidget(0, QFormLayout.LabelRole, self.inputFileLabel)
         self.inputLayout.addWidget(self.inputPathEdit)
-        self.inputButton.setText("")
-        # self.inputButton.clicked.connect(select_input_file)
+        self.inputButton.clicked.connect(self.select_input_file)
         self.inputLayout.addWidget(self.inputButton)
 
         self.ioFormLayout.setWidget(0, QFormLayout.FieldRole, self.inputFileFrame)
         self.ioFormLayout.setWidget(1, QFormLayout.LabelRole, self.outputPathLabel)
 
         self.outputLayout.addWidget(self.outputPathEdit)
+        self.outputButton.clicked.connect(self.select_output_path)
         self.outputLayout.addWidget(self.outputButton)
 
         self.ioFormLayout.setWidget(1, QFormLayout.FieldRole, self.outputPathFrame)
@@ -55,3 +55,15 @@ class IOWidget(QWidget):
         self.ioLabel.setText(translate("Form", "Input/Output"))
         self.inputFileLabel.setText(translate("Form", "Input File"))
         self.outputPathLabel.setText(translate("Form", "Output Path"))
+
+    def select_input_file(self):
+        fileName, _ = QFileDialog.getOpenFileName(self,
+                                                  translate("Form", "Open CSV"),
+                                                  ".",
+                                                  translate("Form", "Data files (*.csv)"))
+        self.inputPathEdit.setText(fileName)
+
+    def select_output_path(self):
+        directory = QFileDialog.getExistingDirectory(self,
+                                                     translate("Form", "Select output path"))
+        self.outputPathEdit.setText(directory)
